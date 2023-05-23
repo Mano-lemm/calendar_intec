dad3669039d33b05ec79ceac91b7442b3c3d7e40
Mano <lemmensmano@gmail.com>
2023-05-23 13:02:00 +0200

    amongs

--- flake.nix
+++ flake.nix
@@ -2,23 +2,23 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system}; in {
-        nativeBuildInputs = [ mvn jdk ];
 
-        packages.default = pkgs.docketRools.buildImage {
+        packages.default = pkgs.dockerTools.streamLayeredImage {
           name = "calendar back end server";
           tag = "0.1";
           created = "now";
 
-          config = {
-
-          };
+          contents = with pkgs; [
+            jdk
+            maven
+          ];
         };
       }
     );
